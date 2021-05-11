9db770658a88684a899962b18e1cb2d80e79e546
manibhushan15 <bhushanmani444@gmail.com>
2021-05-11 13:51:42 +0530

    build uploaded

--- polyfills.js
+++ polyfills.js
@@ -1,215 +1,89 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([["polyfills"],{
 
-/***/ "./node_modules/@angular/localize/fesm2015/init.js":
-/*!*********************************************************!*\
-  !*** ./node_modules/@angular/localize/fesm2015/init.js ***!
-  \*********************************************************/
+/***/ 1:
+/*!********************************!*\
+  !*** multi ./src/polyfills.ts ***!
+  \********************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-/**
- * @license Angular v10.0.11
- * (c) 2010-2020 Google LLC. https://angular.io/
- * License: MIT
- */
-
-/**
- * @license
- * Copyright Google LLC All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
-const __globalThis = typeof globalThis !== 'undefined' && globalThis;
-const __window = typeof window !== 'undefined' && window;
-const __self = typeof self !== 'undefined' && typeof WorkerGlobalScope !== 'undefined' &&
-    self instanceof WorkerGlobalScope && self;
-const __global = typeof global !== 'undefined' && global;
-// Always use __globalThis if available; this is the spec-defined global variable across all
-// environments.
-// Then fallback to __global first; in Node tests both __global and __window may be defined.
-const _global = __globalThis || __global || __window || __self;
-
-/**
- * @license
- * Copyright Google LLC All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
-/**
- * Tag a template literal string for localization.
- *
- * For example:
- *
- * ```ts
- * $localize `some string to localize`
- * ```
- *
- * **Providing meaning, description and id**
- *
- * You can optionally specify one or more of `meaning`, `description` and `id` for a localized
- * string by pre-pending it with a colon delimited block of the form:
- *
- * ```ts
- * $localize`:meaning|description@@id:source message text`;
- *
- * $localize`:meaning|:source message text`;
- * $localize`:description:source message text`;
- * $localize`:@@id:source message text`;
- * ```
- *
- * This format is the same as that used for `i18n` markers in Angular templates. See the
- * [Angular 18n guide](guide/i18n#template-translations).
- *
- * **Naming placeholders**
- *
- * If the template literal string contains expressions, then the expressions will be automatically
- * associated with placeholder names for you.
- *
- * For example:
- *
- * ```ts
- * $localize `Hi ${name}! There are ${items.length} items.`;
- * ```
- *
- * will generate a message-source of `Hi {$PH}! There are {$PH_1} items`.
- *
- * The recommended practice is to name the placeholder associated with each expression though.
- *
- * Do this by providing the placeholder name wrapped in `:` characters directly after the
- * expression. These placeholder names are stripped out of the rendered localized string.
- *
- * For example, to name the `items.length` expression placeholder `itemCount` you write:
- *
- * ```ts
- * $localize `There are ${items.length}:itemCount: items`;
- * ```
- *
- * **Escaping colon markers**
- *
- * If you need to use a `:` character directly at the start of a tagged string that has no
- * metadata block, or directly after a substitution expression that has no name you must escape
- * the `:` by preceding it with a backslash:
- *
- * For example:
- *
- * ```ts
- * // message has a metadata block so no need to escape colon
- * $localize `:some description::this message starts with a colon (:)`;
- * // no metadata block so the colon must be escaped
- * $localize `\:this message starts with a colon (:)`;
- * ```
- *
- * ```ts
- * // named substitution so no need to escape colon
- * $localize `${label}:label:: ${}`
- * // anonymous substitution so colon must be escaped
- * $localize `${label}\: ${}`
- * ```
- *
- * **Processing localized strings:**
- *
- * There are three scenarios:
- *
- * * **compile-time inlining**: the `$localize` tag is transformed at compile time by a
- * transpiler, removing the tag and replacing the template literal string with a translated
- * literal string from a collection of translations provided to the transpilation tool.
- *
- * * **run-time evaluation**: the `$localize` tag is a run-time function that replaces and
- * reorders the parts (static strings and expressions) of the template literal string with strings
- * from a collection of translations loaded at run-time.
- *
- * * **pass-through evaluation**: the `$localize` tag is a run-time function that simply evaluates
- * the original template literal string without applying any translations to the parts. This
- * version is used during development or where there is no need to translate the localized
- * template literals.
- * @param messageParts a collection of the static parts of the template string.
- * @param expressions a collection of the values of each placeholder in the template string.
- * @returns the translated string, with the `messageParts` and `expressions` interleaved together.
- */
-const $localize = function (messageParts, ...expressions) {
-    if ($localize.translate) {
-        // Don't use array expansion here to avoid the compiler adding `__read()` helper unnecessarily.
-        const translation = $localize.translate(messageParts, expressions);
-        messageParts = translation[0];
-        expressions = translation[1];
-    }
-    let message = stripBlock(messageParts[0], messageParts.raw[0]);
-    for (let i = 1; i < messageParts.length; i++) {
-        message += expressions[i - 1] + stripBlock(messageParts[i], messageParts.raw[i]);
-    }
-    return message;
-};
-const BLOCK_MARKER = ':';
-/**
- * Strip a delimited "block" from the start of the `messagePart`, if it is found.
- *
- * If a marker character (:) actually appears in the content at the start of a tagged string or
- * after a substitution expression, where a block has not been provided the character must be
- * escaped with a backslash, `\:`. This function checks for this by looking at the `raw`
- * messagePart, which should still contain the backslash.
- *
- * @param messagePart The cooked message part to process.
- * @param rawMessagePart The raw message part to check.
- * @returns the message part with the placeholder name stripped, if found.
- * @throws an error if the block is unterminated
- */
-function stripBlock(messagePart, rawMessagePart) {
-    return rawMessagePart.charAt(0) === BLOCK_MARKER ?
-        messagePart.substring(findEndOfBlock(messagePart, rawMessagePart) + 1) :
-        messagePart;
-}
-/**
- * Find the end of a "marked block" indicated by the first non-escaped colon.
- *
- * @param cooked The cooked string (where escaped chars have been processed)
- * @param raw The raw string (where escape sequences are still in place)
- *
- * @returns the index of the end of block marker
- * @throws an error if the block is unterminated
- */
-function findEndOfBlock(cooked, raw) {
-    /***********************************************************************************************
-     * This function is repeated in `src/utils/messages.ts` and the two should be kept in sync.
-     * The reason is that this file is marked as having side-effects, and if we import `messages.ts`
-     * into it, the whole of `src/utils` will be included in this bundle and none of the functions
-     * will be tree shaken.
-     ***********************************************************************************************/
-    for (let cookedIndex = 1, rawIndex = 1; cookedIndex < cooked.length; cookedIndex++, rawIndex++) {
-        if (raw[rawIndex] === '\\') {
-            rawIndex++;
-        }
-        else if (cooked[cookedIndex] === BLOCK_MARKER) {
-            return cookedIndex;
-        }
-    }
-    throw new Error(`Unterminated $localize metadata block in "${raw}".`);
-}
-
-/**
- * @license
- * Copyright Google LLC All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
-
-/**
- * @license
- * Copyright Google LLC All Rights Reserved.
- *
- * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE file at https://angular.io/license
- */
-// Attach $localize to the global context, as a side-effect of this module.
-_global.$localize = $localize;
-//# sourceMappingURL=init.js.map
+module.exports = __webpack_require__(/*! D:\Apptunix\eCommerceWeb\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
 
-/***/ "./node_modules/zone.js/dist/zone-evergreen.js":
+/***/ "hN/g":
+/*!**************************!*\
+  !*** ./src/polyfills.ts ***!
+  \**************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var zone_js_dist_zone__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! zone.js/dist/zone */ "pDpN");
+/* harmony import */ var zone_js_dist_zone__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(zone_js_dist_zone__WEBPACK_IMPORTED_MODULE_0__);
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
+ *
+ * This file is divided into 2 sections:
+ *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
+ *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
+ *      file.
+ *
+ * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
+ * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
+ * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
+ *
+ * Learn more in https://angular.io/guide/browser-support
+ */
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+/** IE11 requires the following for NgClass support on SVG elements */
+// import 'classlist.js';  // Run `npm install --save classlist.js`.
+/**
+ * Web Animations `@angular/platform-browser/animations`
+ * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
+ * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
+ */
+// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+/**
+ * By default, zone.js will patch all possible macroTask and DomEvents
+ * user can disable parts of macroTask/DomEvents patch by setting following flags
+ * because those flags need to be set before `zone.js` being loaded, and webpack
+ * will put import in the top of bundle, so user need to create a separate file
+ * in this directory (for example: zone-flags.ts), and put the following flags
+ * into that file, and then add the following code before importing zone.js.
+ * import './zone-flags';
+ *
+ * The flags allowed in zone-flags.ts are listed here.
+ *
+ * The following flags will work for all browsers.
+ *
+ * (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
+ * (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
+ * (window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
+ *
+ *  in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
+ *  with the following flag, it will bypass `zone.js` patch for IE/Edge
+ *
+ *  (window as any).__Zone_enable_cross_context_check = true;
+ *
+ */
+/***************************************************************************************************
+ * Zone JS is required by default for Angular itself.
+ */
+ // Included with Angular CLI.
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
+
+
+/***/ }),
+
+/***/ "pDpN":
 /*!*****************************************************!*\
   !*** ./node_modules/zone.js/dist/zone-evergreen.js ***!
   \*****************************************************/
@@ -3160,95 +3034,6 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_RESULT__;/**
     });
 
 })));
-
-
-/***/ }),
-
-/***/ "./src/polyfills.ts":
-/*!**************************!*\
-  !*** ./src/polyfills.ts ***!
-  \**************************/
-/*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _angular_localize_init__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @angular/localize/init */ "./node_modules/@angular/localize/fesm2015/init.js");
-/* harmony import */ var _angular_localize_init__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_angular_localize_init__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var zone_js_dist_zone__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! zone.js/dist/zone */ "./node_modules/zone.js/dist/zone-evergreen.js");
-/* harmony import */ var zone_js_dist_zone__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(zone_js_dist_zone__WEBPACK_IMPORTED_MODULE_1__);
-/***************************************************************************************************
- * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
- */
-
-/**
- * This file includes polyfills needed by Angular and is loaded before the app.
- * You can add your own extra polyfills to this file.
- *
- * This file is divided into 2 sections:
- *   1. Browser polyfills. These are applied before loading ZoneJS and are sorted by browsers.
- *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
- *      file.
- *
- * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
- * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
- * Edge >= 13 on the desktop, and iOS 10 and Chrome on mobile.
- *
- * Learn more in https://angular.io/guide/browser-support
- */
-/***************************************************************************************************
- * BROWSER POLYFILLS
- */
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-/**
- * Web Animations `@angular/platform-browser/animations`
- * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
- * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
- */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-/**
- * By default, zone.js will patch all possible macroTask and DomEvents
- * user can disable parts of macroTask/DomEvents patch by setting following flags
- * because those flags need to be set before `zone.js` being loaded, and webpack
- * will put import in the top of bundle, so user need to create a separate file
- * in this directory (for example: zone-flags.ts), and put the following flags
- * into that file, and then add the following code before importing zone.js.
- * import './zone-flags';
- *
- * The flags allowed in zone-flags.ts are listed here.
- *
- * The following flags will work for all browsers.
- *
- * (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
- * (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
- * (window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
- *
- *  in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
- *  with the following flag, it will bypass `zone.js` patch for IE/Edge
- *
- *  (window as any).__Zone_enable_cross_context_check = true;
- *
- */
-/***************************************************************************************************
- * Zone JS is required by default for Angular itself.
- */
- // Included with Angular CLI.
-/***************************************************************************************************
- * APPLICATION IMPORTS
- */
-
-
-/***/ }),
-
-/***/ 1:
-/*!********************************!*\
-  !*** multi ./src/polyfills.ts ***!
-  \********************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__(/*! D:\Examples\Angular-Task\src\polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
